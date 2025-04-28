1c66d0c8dd8ec7f2f21e90914bd263311b47bdb3
LacosLu <luka.lacoste49510@gmail.com>
2025-04-28 11:17:51 +0200

    Prise d'image et test

--- test.py
+++ test.py
@@ -1,30 +1,3 @@
-from picamera2 import Picamera2
-import cv2 as cv
+import datetime
 
-cap = cv.VideoCapture(0)
-print(cap)
-
-# Check if camera opened successfully
-print(cap.isOpened())
-if (cap.isOpened() == False):
-    print("Error opening video stream or file")
-
-while cap.isOpened():
-    ret, frame = cap.read()
-    print(ret)
-    print(frame)
-    if ret == True:
-        cv.imshow("frame", frame)
-        k = cv.waitKey(0)
-    else:
-        break
-
-cv.destroyAllWindows()
-cap.release()
-
-"""camera = Picamera2()
-camera.resolution = (1024, 768)
-camera.start_preview()
-# Camera warm-up time
-sleep(2)
-camera.capture_file('foo.jpg')"""
+print((datetime.datetime.strptime("28/04/2025", "%d/%m/%Y").date().strftime("%Y-%m-%d")))
